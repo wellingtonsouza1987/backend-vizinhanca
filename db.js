494d331161bee145d1d01ea89f182521dcdeeec2
Wellington Souza <wellingtonasouza62@gmail.com>
2026-03-25 18:26:55 -0300

    Conectado ao banco Railway

--- db.js
+++ db.js
@@ -1,11 +1,8 @@
 const mysql = require('mysql2');
 
-const conexao = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123456',
-    database: 'vizinhanca'
-});
+const conexao = mysql.createConnection(
+    process.env.MYSQL_URL
+);
 
 conexao.connect((erro) => {
     if (erro) {
