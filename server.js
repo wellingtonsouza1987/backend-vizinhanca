e6b09795535fd7981219022ee9f4c2f0b950ac45
Wellington Souza <wellingtonasouza62@gmail.com>
2026-03-25 19:18:10 -0300

    Atualização na porta do servidor

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.post('/api/ocorrencias', (req, res) => {
     });
 
 // iniciar servidor
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
